af9d99e9c6c2d443557c3255a283337eb2e47751
Kreton16 <renko0708@gmail.com>
2022-11-04 05:11:22 +0200

    f_c

---
+++ UnitTest5.2.1/UnitTest5.2.1.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../pr5.2.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest521
+{
+	TEST_CLASS(UnitTest521)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double a = -2.;
+			A(1, 0, a);
+			Assert::AreEqual(a, 2.);
+		}
+		TEST_METHOD(TestMethod2)
+		{
+			double s = 0;
+			int n = 0;
+			S(1, 1, n, s);
+			Assert::AreEqual(s, 1+(1./3.));
+		}
+
+	};
+}
---
+++ pr5.2.1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+void S(const double x, const double eps, int& n, double& s);
+void A(const double x, const int n, double& a);
+int main()
+{
+	double xp, xk, x, dx, eps, Arth, a = 0, R = 0, s = 0;
+	int n = 0;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+	cout << fixed;
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(10) << "Arth x" << " |"
+		<< setw(7) << "S" << " |"
+		<< setw(5) << "n" << " |"
+		<< endl;
+	cout << "-------------------------------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		S(x, eps, n, s);
+		Arth = (1.0 / 2 * log((1 + x) / (1 - x)));
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(5) << Arth << " |"
+			<< setw(10) << setprecision(5) << s << " |"
+			<< setw(5) << n << " |"
+			<< endl;
+		x += dx;
+	}
+	cout << "-------------------------------------------------" << endl;
+	return 0;
+}
+void S(const double x, const double eps, int& n, double& s)
+{
+	n = 0; // вираз залежить від умови завдання варіанту
+	double a = x; // вираз залежить від умови завдання варіанту
+	s = a;
+	do {
+		n++;
+		A(x, n, a);
+		s += a;
+	} while (abs(a) >= eps);
+}
+void A(const double x, const int n, double& a)
+{
+	double R = (2 * n * x * x - x * x) / (2 * n + 1); // вираз залежить від умови завдання варіанту
+	a *= R;
+}
